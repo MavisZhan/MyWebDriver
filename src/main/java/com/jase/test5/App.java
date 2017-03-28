7337a01ea5ca870d1e8e8e86c30a7ae05bbee489
mavis.zhan <mavis.zhan@bamboo-PC152.bamboonetworks.com>
2017-03-28 12:39:43 +0800

    Create

---
+++ src/main/java/com/jase/test5/App.java
@@ -0,0 +1,13 @@
+package com.jase.test5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/jase/test5/TestGrid1.java
@@ -0,0 +1,39 @@
+package com.jase.test5;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestGrid1 {
+	
+	@BeforeClass
+	public void setUp(){
+		
+	}
+    
+	@Test
+	public void testCase() throws MalformedURLException{
+		WebDriver driver = new RemoteWebDriver(new URL(
+				"http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.chrome());
+
+		driver.get("https://www.baidu.com");
+		driver.findElement(By.id("kw")).sendKeys("selenium");
+		driver.findElement(By.id("su")).click();
+		driver.quit();
+
+
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		
+	}
+
+}
